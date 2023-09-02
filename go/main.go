c6ace17e37adc4f0359b8a0f16d7be56c659bd74
masami <msihima0304@gmail.com>
2023-09-02 21:19:52 +0900

    500msくらい待ってみる

--- go/main.go
+++ go/main.go
@@ -1080,7 +1080,7 @@ var userCreditsCache sync.Map    // キー: courses.id, 値: []struct
 var updateMutex sync.Mutex
 
 func UpdateCacheForCourse(DB *sqlx.DB, courseID string) error {
-	time.Sleep(200 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 	updateMutex.Lock()
 	defer updateMutex.Unlock()
 
