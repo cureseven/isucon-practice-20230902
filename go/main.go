efd987beb75a7472ff8c1d8425a7859911490acb
masami <msihima0304@gmail.com>
2023-09-02 17:47:15 +0900

    JOIN分割した

--- go/main.go
+++ go/main.go
@@ -734,16 +734,25 @@ func (h *handlers) getCourseResults(c echo.Context, registeredCourses []Course, 
 		}
 
 		// この科目を履修している学生のTotalScore一覧を取得
+		enrollmentQuery := "SELECT COUNT(`user_id`) FROM `registrations` WHERE `course_id` = ?"
+
+		// 履修者のTotalScoreを取得するクエリ
+		scoreQuery := "SELECT IFNULL(SUM(`submissions`.`score`), 0) AS `total_score`" +
+			" FROM `submissions`" +
+			" JOIN `classes` ON `classes`.`id` = `submissions`.`class_id`" +
+			" WHERE `classes`.`course_id` = ?" +
+			" GROUP BY `submissions`.`user_id`"
+
+		// Fetch the number of enrolled students
+		var enrollmentCount int
+		if err := h.DB.Get(&enrollmentCount, enrollmentQuery, course.ID); err != nil {
+			c.Logger().Error(err)
+			return nil, 0, 0, c.NoContent(http.StatusInternalServerError)
+		}
+
+		// Fetch the totals
 		var totals []int
-		query := "SELECT IFNULL(SUM(`submissions`.`score`), 0) AS `total_score`" +
-			" FROM `users`" +
-			" JOIN `registrations` ON `users`.`id` = `registrations`.`user_id`" +
-			" JOIN `courses` ON `registrations`.`course_id` = `courses`.`id`" +
-			" LEFT JOIN `classes` ON `courses`.`id` = `classes`.`course_id`" +
-			" LEFT JOIN `submissions` ON `users`.`id` = `submissions`.`user_id` AND `submissions`.`class_id` = `classes`.`id`" +
-			" WHERE `courses`.`id` = ?" +
-			" GROUP BY `users`.`id`"
-		if err := h.DB.Select(&totals, query, course.ID); err != nil {
+		if err := h.DB.Select(&totals, scoreQuery, course.ID); err != nil {
 			c.Logger().Error(err)
 			return nil, 0, 0, c.NoContent(http.StatusInternalServerError)
 		}
