08d3e5162390e67a4e641f53a960186dc5083517
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2023-09-02 13:50:37 +0900

    perf: query修正

--- go/main.go
+++ go/main.go
@@ -714,7 +714,7 @@ func (h *handlers) GetGrades(c echo.Context) error {
 	// GPAの統計値
 	// 一つでも修了した科目がある学生のGPA一覧
 	var gpas []float64
-	if err := h.DB.Select("SELECT gpa from user_gpas", query, StatusClosed, StatusClosed, Student); err != nil {
+	if err := h.DB.Select("SELECT gpa from user_gpas", query); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
