e42666f3f718979a30bcf8f6762bfd876afd868c
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2023-09-02 13:44:36 +0900

    fix: submissionの更新をトリガーにgpaを計算して保持、参照時は計算不要に

--- sql/1_schema.sql
+++ sql/1_schema.sql
@@ -1,4 +1,5 @@
 -- CREATEと逆順
+DROP TABLE IF EXISTS `user_gpas`;
 DROP TABLE IF EXISTS `unread_announcements`;
 DROP TABLE IF EXISTS `announcements`;
 DROP TABLE IF EXISTS `submissions`;
@@ -83,3 +84,43 @@ CREATE TABLE `unread_announcements`
     CONSTRAINT FK_unread_announcements_announcement_id FOREIGN KEY (`announcement_id`) REFERENCES `announcements` (`id`),
     CONSTRAINT FK_unread_announcements_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
+
+CREATE TABLE user_gpas (
+    user_id INT PRIMARY KEY,
+    gpa DECIMAL(5, 2)
+);
+
+DELIMITER //
+
+CREATE TRIGGER after_submission_update
+    AFTER UPDATE ON submissions
+    FOR EACH ROW
+BEGIN
+    DECLARE total_credits DECIMAL(5,2);
+    DECLARE total_score DECIMAL(5,2);
+
+    -- 合計クレジットを計算
+    SELECT SUM(courses.credit) INTO total_credits
+    FROM registrations
+             JOIN courses ON registrations.course_id = courses.id AND courses.status = 'closed'
+    WHERE registrations.user_id = NEW.user_id;
+
+    -- 合計スコアを計算
+    SELECT IFNULL(SUM(submissions.score * courses.credit), 0) INTO total_score
+    FROM registrations
+             JOIN courses ON registrations.course_id = courses.id AND courses.status = 'closed'
+             LEFT JOIN classes ON courses.id = classes.course_id
+             LEFT JOIN submissions ON NEW.user_id = submissions.user_id AND submissions.class_id = classes.id
+    WHERE registrations.user_id = NEW.user_id;
+
+    -- GPAを計算
+    SET @new_gpa = total_score / 100 / total_credits;
+
+    -- GPAをuser_gpaテーブルにupsert
+    INSERT INTO user_gpa (user_id, gpa) VALUES (NEW.user_id, @new_gpa)
+        ON DUPLICATE KEY UPDATE gpa = VALUES(gpa);
+END;
+
+//
+
+DELIMITER ;
