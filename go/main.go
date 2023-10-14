ec9e5c4de73eb75622cd3816600fccfb882f6cbd
masami <msihima0304@gmail.com>
2023-10-14 15:55:00 +0900

    修正

--- go/main.go
+++ go/main.go
@@ -1113,7 +1113,7 @@ func (h *handlers) GetClasses(c echo.Context) error {
 	courseID := c.Param("courseID")
 
 	var count int
-	if err := h.DB.Get(&count, "SELECT insert_count AS COUNTS FROM `class_insert_counts` WHERE `course_id` = ?", courseID); err != nil {
+	if err := h.DB.Get(&count, "SELECT SUM(insert_count) AS COUNTS FROM `class_insert_counts` WHERE `course_id` = ?", courseID); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
